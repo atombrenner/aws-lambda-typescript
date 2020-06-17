d465cffa54cd09049f80cf951b8aa8e521f11c50
Christian Rodemeyer <atombrenner@gmail.com>
2020-06-17 22:48:53 +0200

    automatically execute cdk tests and improve test robustness by moking the lambda code

--- infrastructure/lib/lambda-stack.ts
+++ infrastructure/lib/lambda-stack.ts
@@ -1,5 +1,6 @@
 import { Stack, StackProps, Construct, Duration } from '@aws-cdk/core'
 import { Function, Runtime, Code } from '@aws-cdk/aws-lambda'
+import { Bucket } from '@aws-cdk/aws-s3'
 import { RetentionDays } from '@aws-cdk/aws-logs'
 import { Rule, Schedule } from '@aws-cdk/aws-events'
 import { LambdaFunction } from '@aws-cdk/aws-events-targets'
@@ -8,15 +9,24 @@ export class LambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
 
+    const isTest = process.env.NODE_ENV === 'test'
+
+    const myBucket = new Bucket(this, 'MyBucket', {
+      bucketName: 'some-artifact-bucket',
+    })
+
     const myLambda = new Function(this, 'MyLambdaFunction', {
       functionName: 'MyLambdaFunction',
       runtime: Runtime.NODEJS_12_X,
       handler: 'lambda.handler',
-      code: Code.fromAsset('../dist/lambda.zip'),
+      code: isTest ? Code.fromInline('test') : Code.fromAsset('../dist/lambda.zip'),
       memorySize: 256,
       timeout: Duration.minutes(5),
       logRetention: RetentionDays.ONE_WEEK, // this will trigger the creation of a custom lambda used by CDK which looks strange too me
     })
+
+    myBucket.grantRead(myLambda)
+    myBucket.grantDelete(myLambda)
 
     new Rule(this, 'MySchedule', {
       schedule: Schedule.rate(Duration.days(1)),
