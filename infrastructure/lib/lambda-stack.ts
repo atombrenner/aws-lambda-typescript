a4a1900e7e55e49b1d7aa134d6ea47732ded1601
Christian Rodemeyer <atombrenner@gmail.com>
2020-06-18 08:49:03 +0200

    describe aws resources

--- infrastructure/lib/lambda-stack.ts
+++ infrastructure/lib/lambda-stack.ts
@@ -11,12 +11,9 @@ export class LambdaStack extends Stack {
 
     const isTest = process.env.NODE_ENV === 'test'
 
-    const myBucket = new Bucket(this, 'MyBucket', {
-      bucketName: 'some-artifact-bucket',
-    })
-
-    const myLambda = new Function(this, 'MyLambdaFunction', {
+    const myLambda = new Function(this, 'MyLambdaFunctionId', {
       functionName: 'MyLambdaFunction',
+      description: 'Some description of my lambda function',
       runtime: Runtime.NODEJS_12_X,
       handler: 'lambda.handler',
       code: isTest ? Code.fromInline('test') : Code.fromAsset('../dist/lambda.zip'),
@@ -25,10 +22,14 @@ export class LambdaStack extends Stack {
       logRetention: RetentionDays.ONE_WEEK, // this will trigger the creation of a custom lambda used by CDK which looks strange too me
     })
 
+    const myBucket = new Bucket(this, 'MyBucketId', {
+      bucketName: 'some-artifact-bucket',
+    })
     myBucket.grantRead(myLambda)
     myBucket.grantDelete(myLambda)
 
-    new Rule(this, 'MySchedule', {
+    new Rule(this, 'MyScheduleId', {
+      description: 'Triggers my lambda function on some schedule',
       schedule: Schedule.rate(Duration.days(1)),
       targets: [new LambdaFunction(myLambda)],
     })
