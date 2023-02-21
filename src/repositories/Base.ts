eba52ed80cfa294b512673c7e1929e6f626e923d
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2023-02-21 10:40:07 -0300

    create get items result interface

--- src/repositories/Base.ts
+++ src/repositories/Base.ts
@@ -1,7 +1,13 @@
 export default interface Repository {
     createOrUpdateItem(id: number, object: object, tableName: string): Promise<void>,
     deleteItem(id: number, tableName: string): Promise<void>,
-    getAllItems(tableName: string): Promise<Item[]>
+    getAllItems(tableName: string): Promise<Item[]>,
+    getItems(tableName: string, exclusiveStartKey?: string): Promise<GetItemsResult>
+}
+
+export interface GetItemsResult {
+    items: Item[],
+    startKey?: string
 }
 
 export interface Item {
