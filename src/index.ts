e0ec7440a531b6e069348aaa2b128bb83e416528
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2023-02-21 09:51:49 -0300

    create update route and implement controller and service

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import Router from 'lambda-router-typescript';
 import type { APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2, Context } from 'aws-lambda';
+import UpdateController from './controllers/Update';
+import UpdateService from './services/Update';
+import MondayGQLGateway from './gateways/MondayGQL';
+import DynamoDB from './repositories/DynamoDB';
 
 type LambdaFunctionUrlEvent = APIGatewayProxyEventV2;
 type LambdaFunctionUrlResult = APIGatewayProxyStructuredResultV2;
@@ -8,8 +12,16 @@ export async function handler(
     event: LambdaFunctionUrlEvent,
     context: Context
 ): Promise<LambdaFunctionUrlResult> {
+    const mondayGateway = new MondayGQLGateway();
+    const repository = new DynamoDB();
+
+    const updateService = new UpdateService(mondayGateway, repository);
+
+    const updateController = new UpdateController(updateService);
 
     const router = new Router();
+
+    router.post("/update", (event) => updateController.handle(event as LambdaFunctionUrlEvent));
 
     try {
         const response = await router.call(event);
