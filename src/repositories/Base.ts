a73f9ddc46193e38046c85f8ffe087b420d4b449
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2023-02-21 08:54:53 -0300

    create item type

--- src/repositories/Base.ts
+++ src/repositories/Base.ts
@@ -1,5 +1,10 @@
 export default interface Repository {
     createOrUpdateItem(id: number, object: object, tableName: string): Promise<void>,
     deleteItem(id: number, tableName: string): Promise<void>,
-    getAllItems(tableName: string): Promise<object[]>
+    getAllItems(tableName: string): Promise<Item[]>
+}
+
+export interface Item {
+    id: number,
+    [key: string]: any
 }
