190b42c99d498a66c3403e378e16d3bc9853bf05
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2023-02-21 10:39:31 -0300

    create get service

---
+++ src/services/Get.ts
@@ -0,0 +1,15 @@
+import { DDB_TABLE_NAME, MONDAY_BOARD_ID } from "../constants";
+import Repository, { GetItemsResult } from "../repositories/Base";
+
+export default class GetService {
+
+    constructor(
+        private readonly repository: Repository
+    ) {
+    }
+
+    async execute(startKey?: string): Promise<GetItemsResult> {
+        const items = await this.repository.getItems(DDB_TABLE_NAME, startKey);
+        return items;
+    }
+}
