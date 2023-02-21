69ffa07d2259228ca3e8ea6f2b605a44d5b9925b
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2023-02-21 08:56:23 -0300

    implement router

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
-import type { APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2, Context } from 'aws-lambda'
+import Router from 'lambda-router-typescript';
+import type { APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2, Context } from 'aws-lambda';
 
-type LambdaFunctionUrlEvent = APIGatewayProxyEventV2
-type LambdaFunctionUrlResult = APIGatewayProxyStructuredResultV2
+type LambdaFunctionUrlEvent = APIGatewayProxyEventV2;
+type LambdaFunctionUrlResult = APIGatewayProxyStructuredResultV2;
 
 export async function handler(
     event: LambdaFunctionUrlEvent,
     context: Context
 ): Promise<LambdaFunctionUrlResult> {
-    return {
-        statusCode: 200
+
+    const router = new Router();
+
+    try {
+        const response = await router.call(event);
+        return response;
+    } catch (error: any) {
+        return {
+            statusCode: error.statusCode || 500,
+            body: JSON.stringify(error.body) || "Internal error."
+        };
     }
+
 }
