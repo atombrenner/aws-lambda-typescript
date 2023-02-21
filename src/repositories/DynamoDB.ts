9377a79025a631a39cbf1d650200554e54778929
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2023-02-21 10:39:51 -0300

    create get dynamo

--- src/repositories/DynamoDB.ts
+++ src/repositories/DynamoDB.ts
@@ -1,6 +1,6 @@
 import AWS from 'aws-sdk';
 
-import Repository, { Item } from "./Base";
+import Repository, { GetItemsResult, Item } from "./Base";
 
 export default class DynamoDB implements Repository {
 
@@ -40,7 +40,7 @@ export default class DynamoDB implements Repository {
         let exclusiveStartKey = null;
 
         do {
-            const scanResult: AWS.DynamoDB.DocumentClient.ScanOutput = await this.getItems(tableName, exclusiveStartKey || undefined);
+            const scanResult: AWS.DynamoDB.DocumentClient.ScanOutput = await this.scanItems(tableName, exclusiveStartKey || undefined);
             hasPagination = !!scanResult.LastEvaluatedKey;
             exclusiveStartKey = scanResult.LastEvaluatedKey;
 
@@ -54,7 +54,33 @@ export default class DynamoDB implements Repository {
         return items;
     }
 
-    private getItems(tableName: string, exclusiveStartKey?: object) {
+    async getItems(tableName: string, exclusiveStartKey?: string): Promise<GetItemsResult> {
+        const exclusiveStartKeyObject = {
+            id: exclusiveStartKey || undefined
+        };
+
+        const scanResult: AWS.DynamoDB.DocumentClient.ScanOutput = await this.scanItems(tableName, exclusiveStartKeyObject);
+
+        if (!scanResult.Items) return {
+            items: []
+        };
+
+        const rawItems = [...scanResult.Items];
+        const mappedItems: Item[] = rawItems.map(item => ({ ...item, id: item.id }));
+
+        if (scanResult.LastEvaluatedKey) {
+            return {
+                items: mappedItems,
+                startKey: scanResult.LastEvaluatedKey.id
+            };
+        }
+
+        return {
+            items: mappedItems
+        };
+    }
+
+    private scanItems(tableName: string, exclusiveStartKey?: object) {
         return this.documentClient.scan({
             TableName: tableName,
             ExclusiveStartKey: exclusiveStartKey
